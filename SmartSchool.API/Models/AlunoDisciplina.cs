28ee4aba05e8c20db20cda6dc72ac438f073b7c5
Dênner Rondinely <denner.rondinely@bizcap.com.br>
2020-08-21 18:43:19 -0300

    Adicionar models

--- SmartSchool.API/Models/AlunoDisciplina.cs
+++ SmartSchool.API/Models/AlunoDisciplina.cs
@@ -2,6 +2,15 @@ namespace SmartSchool.API.Models
 {
     public class AlunoDisciplina
     {
-        
+        public AlunoDisciplina() { }
+        public AlunoDisciplina(int alunoID, int disciplinaId)
+        {
+            this.AlunoID = alunoID;
+            this.DisciplinaId = disciplinaId;
+        }
+        public int AlunoID { get; set; }
+        public Aluno Aluno { get; set; }
+        public int DisciplinaId { get; set; }
+        public Disciplina Disciplina { get; set; }
     }
 }
